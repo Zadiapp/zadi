fe5e0e2021bd6441853a4ea89ee94234afdc456e
Ahmed Alawady <ahmedlaravel@gmail.com>
2018-07-28 15:03:55 +0200

    Create migration and model suggestion markets

---
+++ database/migrations/2018_07_28_125742_create-suggestion-markets.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSuggestionMarkets extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('suggestion_markets', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('email')->nullable();
+            $table->string('opening_hour')->nullable();
+            $table->string('mobile')->unique()->nullable();
+            $table->string('phone')->unique()->nullable();
+            $table->string('address')->nullable();
+            $table->string('min_charge')->nullable();
+            $table->string('note')->nullable();
+            $table->integer('status')->nullable();
+            $table->string('user_id')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_general_ci';
+            $table->foreign('user_id')->references('id')->on('users');
+        });
+
+        DB::statement('ALTER TABLE suggestion_markets ADD location POINT null');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('suggestion_markets', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+        });
+
+        Schema::dropIfExists('suggestion_markets');
+    }
+}
