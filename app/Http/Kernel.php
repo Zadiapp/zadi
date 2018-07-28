32356b941f945eb3edbab4d526c3edd60caa73d2
Ahmed Alawady <ahmedlaravel@gmail.com>
2018-07-28 11:20:59 +0200

    Wrap JWT token error

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -52,7 +52,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'jwt.auth' => 'Tymon\JWTAuth\Middleware\GetUserFromToken',
+        'jwt.auth' => \App\Http\Middleware\JWTCheck::class,
         'jwt.refresh' => 'Tymon\JWTAuth\Middleware\RefreshToken',
     ];
 }
