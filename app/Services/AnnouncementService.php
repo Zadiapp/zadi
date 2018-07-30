b320053a4505cfe8fb7da343fc42937adbace437
Ahmed Alawady <ahmedlaravel@gmail.com>
2018-07-30 23:41:50 +0200

    List announcements

--- app/Services/AnnouncementService.php
+++ app/Services/AnnouncementService.php
@@ -5,7 +5,8 @@ namespace App\Services;
 class AnnouncementService{
 
     function getMarketAnnouncement($marketId) {
-        $announcement = \App\Models\Announcement::where('market_id', $marketId)->pluck('text');
+        $announcement = \App\Models\Announcement::where('market_id', $marketId)
+            ->where('status', \Config::get('constants.announcementStatus.active'))->pluck('text');
 
         if(isset($announcement[0])) {
             return $announcement[0];
